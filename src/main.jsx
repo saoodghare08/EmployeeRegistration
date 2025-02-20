73139d9f36d9936352b66dd59c7e9761304a2857
Saood Ghare <saood.g@automatebudy.com>
2025-02-20 17:52:35 +0530

    build fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import configureStore from "./application/store.jsx";
-import services from "./infrastructure/services/index.jsx";
+import services from "./infrastructure/Services/index.jsx";
 import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById("root")).render(
